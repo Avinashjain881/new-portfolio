3224410e197ba48a54a0f5aa791936b2e2668f01
avinashcodemymobile <avinash@codemymobile>
2019-08-13 13:55:15 +0530

    full-portfolio-without-my-details

--- main.js
+++ main.js
@@ -61,3 +61,12 @@ btn.on('click', function(e) {
   $('html, body').animate({scrollTop:0}, '300');
 });
 
+function openNav() {
+	document.getElementById("mySidebar").style.width = "250px";
+	document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+	document.getElementById("mySidebar").style.width = "0";
+	document.getElementById("main").style.marginLeft= "0";
+  }
