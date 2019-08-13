c46213039a14fb5cf77098a210d94d4673f9ac2e
avinashcodemymobile <avinash@codemymobile>
2019-08-13 12:18:36 +0530

    almost-full-portfolio

--- main.js
+++ main.js
@@ -45,3 +45,19 @@ typingEffect();
 	  }, index * 1000);
 	});
   })(document)
+
+  var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
